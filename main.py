a5b43b6b18e5f216d0a66745eb2dc287e2cd1758
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2022-12-14 16:33:31 +0100

    Sintetizador_Concatenativo_de_Difonos v0.1. Created first function in main to open the Praat Script save_labeled_intervals_to_wav_sound_files.praat and run it, with the needed parameters and objects. It saves the resulting objects and also returns them.

--- main.py
+++ main.py
@@ -21,16 +21,60 @@ prosodia.
 #                                   Libraries
 import os
 
+# to use praat
+import parselmouth
+from parselmouth import Sound, praat, TextGrid
+
 #                                   Constants
-corpus_folder = "corpus"
-auxiliar_folder = "auxiliar"
+corpus_folder = "corpus/"
+auxiliar_folder = "auxiliar/"
 labeled_intervals_to_wav = "save_labeled_intervals_to_wav_sound_files.praat"
 
-variable = "test" # variable de test para probar git push
-
-def callPraatScript(file_name, script_name=auxiliar_folder + labeled_intervals_to_wav):
+# todo generalizar funcion para cualquier script -> meter todos los datos en listas para que corra de cualquier manera
+def callPraatScript(file_name, script_name):
     # Check that all the parameters exists
-    assert os.path.exists(file_name) and os.path.exists(script_name)
+    assert os.path.exists(file_name) and os.path.exists(script_name), "ERROR"
 
+    # constant parameters for the script
+    tier = 1  # (int) Which IntervalTier in this TextGrid would you like to process?
+    Start_from = 1  # (int) Starting at which interval?
+    End_at = 0  # (int) ending at which interval?
+    Exclude_empty_labels = True  # (boolean)
+    Exclude_intervals_labeled_as_xxx = True  # (boolean)
+    Exclude_intervals_starting_with_dot = True  # (boolean)
+    positive_Margin = 0.0001  # (float) Give a small margin for the files if you like (seconds)
+    sentence_Folder = "corpus/"  # Give the folder where to save the sound files
+    sentence_Prefix = "TMP_"  # Give an optional prefix for all filenames:
+    sentence_Suffix = ""  # Give an optional suffix for all filenames (.wav will be added anyway):
 
-    os.system("Praat.exe " + script_name + " " + file_name)
+    # parameters = [tier, Start_from, End_at, Exclude_empty_labels, Exclude_intervals_labeled_as_xxx,
+    #              Exclude_intervals_starting_with_dot, positive_Margin, sentence_Folder, sentence_Prefix,
+    #              sentence_Suffix]
+
+    snd = Sound(file_name)
+    # snd.save("tests/test.wav","WAV")
+
+    tgt = TextGrid(0, 1, "Mary John bell", "bell")
+    # tgt.save("tests/test.TextGrid")
+
+    objects = [tgt, snd]
+
+    # So the everything before the script (script_name) are selected objects, Everything after are arguments to this form
+    resulting_objects = praat.run_file(objects, script_name, tier, Start_from, End_at, Exclude_empty_labels,
+                                       Exclude_intervals_labeled_as_xxx, Exclude_intervals_starting_with_dot,
+                                       positive_Margin, sentence_Folder, sentence_Prefix, sentence_Suffix)
+
+    # assert that resulting_objects its not empty
+    assert resulting_objects
+
+    # if we want to save the results in the folder tests/Results_idx + .TextGrid
+    for idx, resulting_object in enumerate(resulting_objects):
+        resulting_objects[idx].save("tests/result_" + str(idx) + ".TextGrid")
+
+    return resulting_objects
+
+# test the function
+file_name = "auxiliar/manipular-pitch/12345.wav"
+script_name = "auxiliar/save_labeled_intervals_to_wav_sound_files.praat"
+
+resulting_objects = callPraatScript(file_name, script_name)
