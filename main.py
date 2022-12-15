7170c4d7e9e62a4b53e09bf8d716d4a66850c469
albertofermer <albertofernmer@gmail.com>
2022-12-15 12:32:00 +0100

    created "Frases para Extracción de Difonos" folder with .mp3 and .marks files.

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ prosodia.
         ii. no reproducir automáticamente el audio.
 '''
 
+# Instalación de bibliotecas
+
 #                                                       Libraries
 import os
 
@@ -30,13 +32,14 @@ from parselmouth import Sound, praat, TextGrid
 #                               DICTIONARY
 
 # Phones
-v = ["e"]   # vowel -> if with accent = E, without accent = e
+v = ["e"]  # vowel -> if with accent = E, without accent = e
 c = ["f", "k", "m", "R", "s", "t"]  # consonants
 
 # syllables
 cv = ["fe", "ke", "me", "re", "se", "te"]
-ccv = ["fre", "kre", "tre"]     # f, k , t + liquid consonant r + vowel (e)
-vc_cvc_ccvc = ["es", "fes", "kes", "mes", "res", "ses", "tes", "fre", "kre", "tre"]    # resultantes de agregar [s] final a los tres tipos anteriores: [es], [fes],[fres], etc.
+ccv = ["fre", "kre", "tre"]  # f, k , t + liquid consonant r + vowel (e)
+vc_cvc_ccvc = ["es", "fes", "kes", "mes", "res", "ses", "tes", "fre", "kre",
+               "tre"]  # resultantes de agregar [s] final a los tres tipos anteriores: [es], [fes],[fres], etc.
 
 dictionary = v + cv + ccv + vc_cvc_ccvc
 
@@ -44,7 +47,7 @@ dictionary = v + cv + ccv + vc_cvc_ccvc
 #       - sound r cannot be initial in a sentence
 #       - sound of r cannot follow an [s]
 
-# one instance of each diphone -> no unit selection when syntetising a new sentence
+# one instance of each diphone -> no unit selection when synthesising a new sentence
 
 # ==========================================================================
 
@@ -52,34 +55,35 @@ corpus_folder = "corpus/"
 auxiliar_folder = "auxiliar/"
 labeled_intervals_to_wav = "save_labeled_intervals_to_wav_sound_files.praat"
 
+
 #                                                   Auxiliar Functions
 
 # todo generalizar funcion para cualquier script -> meter todos los datos en listas para que corra de cualquier manera
 def callPraatScript(file_name, script_name):
-    '''
+    """
     callPraatScript
 
-    call the Praat Script script_name and runs it on the sound file file_name. It is runned in one by one sound file, so
+    call the Praat Script script_name and runs it on the sound file file_name. It is run in one by one sound file, so
     if you want to run it on several sound files, just call it several times on different sound files.
 
     :param file_name: path + name of the sound file (.wav)
     :param script_name: path + name of the script file (.praat)
     :return: resulting_objects: list of objects results of the script
-    '''
+    """
     # Check that all the parameters exists
     assert os.path.exists(file_name) and os.path.exists(script_name), "ERROR"
 
     # constant parameters for the script
     tier = 1  # (int) Which IntervalTier in this TextGrid would you like to process?
-    Start_from = 1  # (int) Starting at which interval?
-    End_at = 0  # (int) ending at which interval?
-    Exclude_empty_labels = True  # (boolean)
-    Exclude_intervals_labeled_as_xxx = True  # (boolean)
-    Exclude_intervals_starting_with_dot = True  # (boolean)
-    positive_Margin = 0.0001  # (float) Give a small margin for the files if you like (seconds)
-    sentence_Folder = "corpus/"  # Give the folder where to save the sound files
-    sentence_Prefix = "TMP_"  # Give an optional prefix for all filenames:
-    sentence_Suffix = ""  # Give an optional suffix for all filenames (.wav will be added anyway):
+    start_from = 1  # (int) Starting at which interval?
+    end_at = 0  # (int) ending at which interval?
+    exclude_empty_labels = True  # (boolean)
+    exclude_intervals_labeled_as_xxx = True  # (boolean)
+    exclude_intervals_starting_with_dot = True  # (boolean)
+    positive_margin = 0.0001  # (float) Give a small margin for the files if you like (seconds)
+    sentence_folder = "corpus/"  # Give the folder where to save the sound files
+    sentence_prefix = "TMP_"  # Give an optional prefix for all filenames:
+    sentence_suffix = ""  # Give an optional suffix for all filenames (.wav will be added anyway):
 
     # parameters = [tier, Start_from, End_at, Exclude_empty_labels, Exclude_intervals_labeled_as_xxx,
     #              Exclude_intervals_starting_with_dot, positive_Margin, sentence_Folder, sentence_Prefix,
@@ -93,12 +97,13 @@ def callPraatScript(file_name, script_name):
 
     objects = [tgt, snd]
 
-    # So the everything before the script (script_name) are selected objects, Everything after are arguments to this form
-    resulting_objects = praat.run_file(objects, script_name, tier, Start_from, End_at, Exclude_empty_labels,
-                                       Exclude_intervals_labeled_as_xxx, Exclude_intervals_starting_with_dot,
-                                       positive_Margin, sentence_Folder, sentence_Prefix, sentence_Suffix)
+    # So everything before the script (script_name) are selected objects, everything after are arguments to this
+    # form
+    resulting_objects = praat.run_file(objects, script_name, tier, start_from, end_at, exclude_empty_labels,
+                                       exclude_intervals_labeled_as_xxx, exclude_intervals_starting_with_dot,
+                                       positive_margin, sentence_folder, sentence_prefix, sentence_suffix)
 
-    # assert that resulting_objects its not empty
+    # assert that resulting_objects is not empty
     assert resulting_objects
 
     # if we want to save the results in the folder tests/Results_idx + .TextGrid
@@ -107,10 +112,10 @@ def callPraatScript(file_name, script_name):
 
     return resulting_objects
 
+
 # test the function
 file_name = "auxiliar/manipular-pitch/12345.wav"
 script_name = "auxiliar/save_labeled_intervals_to_wav_sound_files.praat"
 
-resulting_objects = callPraatScript(file_name, script_name)
-
-
+resulting_textgrid = callPraatScript(file_name, script_name)
+print(resulting_textgrid)
