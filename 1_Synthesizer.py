9a23193cdd67d98d93a9fcb20a8915300237a8d4
GrunCrow <57197254+GrunCrow@users.noreply.github.com>
2022-12-16 13:20:33 +0100

    Sintetizador_Concatenativo_de_Difonos v1.1.
    Synthesizer file created and started

---
+++ 1_Synthesizer.py
@@ -0,0 +1,16 @@
+'''
+4. TODO Crear un programa que, dada una secuencia de fonos, concatene los archivos de los
+    difonos correspondientes, genere un archivo wav y (de ser necesario) modifique su
+    prosodia.
+
+    • El programa debe funcionar en modo batch (no interactivo), recibiendo como
+    únicos argumentos la secuencia de fonos a sintetizar y el nombre del archivo
+    wav a crear. Ejemplo:
+
+    python tts.py EsemeketrEfe? /tmp/output.wav
+
+    • La salida debe guardarse como un archivo wav (mono, 16kHz, 16 bits).
+    • TODO El programa tendrá además dos opciones
+        i.  Reproducir automáticamente el audio
+        ii. no reproducir automáticamente el audio.
+'''
